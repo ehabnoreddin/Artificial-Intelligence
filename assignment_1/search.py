bcc3926b3d33a2abe37a9a3db6f0013ec25cad9b
Corey Prophitt <prophitt.corey@gmail.com>
2012-08-25 11:54:40 -0400

    Refactored code to fit the assignment specifications.

--- assignment_1/search.py
+++ assignment_1/search.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
 '''
-	File: 	assignment_1.py
-	Author: Corey Prophitt <prophitt.corey@gmail.com>
-	Class:	CS440
-	License: GPLv3, see license.txt for more details.
+	File: 	    search.py
+	Author:     Corey Prophitt <prophitt.corey@gmail.com>
+	Class:	    CS440
+	License:    GPLv3, see license.txt for more details.
 '''
 
 
@@ -15,62 +15,10 @@
 # Let's try to maintain Python 3 portability..
 from __future__ import print_function
 
-from os import name as os_name
-
-from sys import exit
 
 #
-# Custom Library Imports
+# The graph search algorithms (BFS, DFS)
 #
-
-try:
-    # My own custom logging utility
-    from pyutils.logger import (DummyColorProvidor, 
-                                UnixColorProvidor, 
-                                ConsoleLogger)
-except ImportError:
-    exit('Problem importing logger.py, is the file/folder missing?')
-
-try:
-	# My own debugging utilities
-	from pyutils.debug import debug
-except:
-	exit('Problem importing debug.py, is the file/folder missing?')
-
-	
-#
-# Setup Logging
-#
-
-if os_name is 'posix':
-    color_providor = UnixColorProvidor()
-else:
-    # No colors on Windows yet, sorry!
-    color_providor = DummyColorProvidor()
-
-log = ConsoleLogger(color_providor)
-
-
-#
-# The Graph(s)
-#
-
-             
-def successorsf(graph, node):
-
-	log.debug_write('Entering successorsf.')
-    
-	if graph is None:
-		log.debug_write('Graph is None. Returning None.', 'ERROR')
-		return None
-
-	if node not in graph:
-		log.debug_write('Node not found, returning empty list.', 'EVENT')
-		return list()
-
-	log.debug_write('Node found, returning its children.', 'EVENT')
-	return graph[node]
-
 
 def bfs(graph, start_node, end_node):
 
@@ -93,52 +41,15 @@ def bfs(graph, start_node, end_node):
 	return visited
 
 
+#
+# Main (individual tests)
+#
+
 if __name__ == '__main__':
 
 	# Used for unit testing the current module when run as main 
 	from unittest import main, TestCase
-
 	
-	log.debug_write('Starting Unit Tests..', 'NOTICE')
-
-	#
-	# Successorsf Tests
-	#
-	
-	class SuccessorsfTests(TestCase):
-
-		def setUp(self):
-
-			# The graph.. Node -> Accessible Node
-			self.graph = {'a':  ['b', 'c', 'd'],
-						  'b':  ['e', 'f', 'g'],
-						  'c':  ['a', 'h', 'i'],
-						  'd':  ['j', 'z'],
-						  'e':  ['k', 'l'],
-						  'g':  ['m'],
-						  'k':  ['z']}
-
-		def tearDown(self):
-			pass
-
-		def test_no_node_in_graph(self):
-			node = 'z' 
-			successors = successorsf(self.graph, node)
-			expected = [] # Empty list when node doesn't exist!
-			self.assertEquals(successors, expected)      
-
-		def test_node_in_graph(self):
-			node = 'a' 
-			successors = successorsf(self.graph, node)
-			expected = ['b', 'c', 'd']
-			self.assertEquals(successors, expected)      
-
-		def test_null_graph(self):
-			node = 'a' 
-			successors = successorsf(None, node)
-			expected = None
-			self.assertEquals(successors, expected)      
-
 			
 	#
 	# BFS Tests
@@ -170,6 +81,7 @@ if __name__ == '__main__':
 			
 		def test_not_a_valid_node(self):
 			pass
+
 			
 	#
 	# Run all tests and exit
--- assignment_1/simpleGraph.py
+++ assignment_1/simpleGraph.py
@@ -1,5 +1,153 @@
-import matplotlib.pyplot as plt
+#!/usr/bin/env python
 
-plt.ion()
+'''
+	File: 	    simpleGraph.py
+	Author:     Corey Prophitt <prophitt.corey@gmail.com>
+	Class:	    CS440
+	License:    GPLv3, see license.txt for more details.
+'''
 
-plt.plot(range(10), 'o-')
+
+#
+# Standard Library Imports
+#
+
+# Let's try to maintain Python 3 portability..
+from __future__ import print_function
+
+from os import name as os_name
+
+from sys import exit
+
+
+#
+# Custom Library Imports
+#
+
+try:
+    # My own custom logging utility
+    from pyutils.logger import (DummyColorProvidor, 
+                                UnixColorProvidor, 
+                                ConsoleLogger)
+except ImportError:
+    exit('Problem importing logger.py, is the file/folder missing?')
+
+try:
+    # My own debugging utilities
+    from ..pyutils.debug import debug
+except ImportError:
+    exit('Problem importing debug.py, is the file/folder missing?')
+
+try:
+    from search import bfs, dfs
+except ImportError:
+    exit('Problem importing search.py, is the file/folder missing?')
+
+
+#
+# Setup Logging
+#
+
+if os_name is 'posix':
+    color_providor = UnixColorProvidor()
+else:
+    # No colors on Windows yet, sorry!
+    color_providor = DummyColorProvidor()
+
+log = ConsoleLogger(color_providor)
+
+
+#
+# The Graph
+#
+
+successors = {'a':  ['b', 'c', 'd'],
+              'b':  ['e', 'f', 'g'],
+              'c':  ['a', 'h', 'i'],
+              'd':  ['j', 'z'],
+              'e':  ['k', 'l'],
+              'g':  ['m'],
+              'k':  ['z']}
+
+             
+def successorsf(graph, node):
+    '''
+    Input:
+             A 'graph' represented as a Python dictionary of
+             node -> children pairs.
+            
+             A 'node' representing the node you are looking up.
+            
+    Output:
+             A list of children of the given 'node'.
+             
+             Returns an empty list if the 'node' doesn't exist, or if the node
+             has no children.
+
+             Returns None if the graph is None.
+    '''
+	if graph is None:
+		return None
+
+	if node not in graph:
+		return list()
+
+	return graph[node]
+
+
+if __name__ == '__main__':
+    
+    log.write('Starting assignment example..')
+    
+    log.write('Finished assignment example.')
+
+    print('\n\n')
+
+
+    # Used for unit testing the current module when run as main 
+    from unittest import main, TestCase
+    
+    
+    #
+    # Successorsf Tests
+    #
+    
+    class SuccessorsfTests(TestCase):
+
+            def setUp(self):
+
+                    # The graph.. Node -> Accessible Node
+                    self.graph = {'a':  ['b', 'c', 'd'],
+                                  'b':  ['e', 'f', 'g'],
+                                  'c':  ['a', 'h', 'i'],
+                                  'd':  ['j', 'z'],
+                                  'e':  ['k', 'l'],
+                                  'g':  ['m'],
+                                  'k':  ['z']}
+
+            def tearDown(self):
+                    pass
+
+            def test_no_node_in_graph(self):
+                    node = 'z' 
+                    successors = successorsf(self.graph, node)
+                    expected = [] # Empty list when node doesn't exist!
+                    self.assertEquals(successors, expected)      
+
+            def test_node_in_graph(self):
+                    node = 'a' 
+                    successors = successorsf(self.graph, node)
+                    expected = ['b', 'c', 'd']
+                    self.assertEquals(successors, expected)      
+
+            def test_null_graph(self):
+                    node = 'a' 
+                    successors = successorsf(None, node)
+                    expected = None
+                    self.assertEquals(successors, expected)
+
+    #
+    # Run all tests and exit
+    #
+    
+    main()

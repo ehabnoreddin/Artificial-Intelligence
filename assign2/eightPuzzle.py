72a4e5d1481762f4b2722373418b8baeff506c4e
Corey Prophitt <prophitt.corey@gmail.com>
2012-09-17 20:01:09 -0400

    Put down basic stubs and impls.

--- assign2/eightPuzzle.py
+++ assign2/eightPuzzle.py
@@ -11,6 +11,104 @@
             The famous 8-puzzle solved using the iterative deepening algorithm.
 '''
 
+#
+# Standard module improts
+#
+from copy import copy
+
+
+def printState(state):
+    '''Prints a state in a more human friendly way.'''
+    return "{0}{1}{2}\n{3}{4}{5}\n{6}{7}{8}".format(state[0],
+                                                    state[1],
+                                                    state[2],
+                                                    state[3],
+                                                    state[4],
+                                                    state[5],
+                                                    state[6],
+                                                    state[7],
+                                                    state[8],)
+    
+def actionsF(state):
+    '''Returns a list of all possible actions from the current state. The input
+    is the state, the output is a list of all possible actions from the given
+    state. Returns an empty list if no action is possible.'''
+    
+    # Only test if the size is 9, and
+    # if a zero (space) exists, else it's bad so return an empty list..
+    if len(state) == 9 and 0 in state:
+        zeroPos = state.index(0)
+        if zeroPos == 0:
+            return ['right', 'down']
+        if zeroPos == 1:
+            return ['left','right', 'down']
+        if zeroPos == 2:
+            return ['left', 'down']
+        if zeroPos == 3:
+            return ['right','up','down']
+        if zeroPos == 4:
+            return ['left','right','up', 'down']    
+        if zeroPos == 5:
+            return ['left','up','down']
+        if zeroPos == 6:
+            return ['right','up']
+        if zeroPos == 7:
+            return ['left','right','up']
+        if zeroPos == 8:
+            return ['left','up'] 
+    return []
+    
+    
+def takeActionF(state, action):
+    '''Takes an action and performs that on the given state. Possible actions
+    include 'up' ,'down', 'left' and 'right'. These move the space around the
+    puzzle board. The output is the new state which is a result of the action.
+    '''
+    
+    actions = actionsF(state)
+    if action not in actions:
+        # Returns an empty list, action requested was impossible..
+        return []
+        
+    zeroPos = state.index(0)
+    if action == 'left':
+        state[zeroPos], state[zeroPos - 1] = state[zeroPos - 1], state[zeroPos]
+        return state
+    
+    if action == 'right':
+        state[zeroPos], state[zeroPos + 1] = state[zeroPos + 1], state[zeroPos]
+        return state
+    
+    return []
+
 
 if __name__ == '__main__':
-    pass
+    # Test printState as shown in class assignment details    
+    state = [1, 0, 3, 4, 2, 5, 6, 7, 8]
+    assert(printState(state) == "103\n425\n678")
+    state = [1,2,3, 4,0,5, 6,7,8]
+    assert(printState(state) == "123\n405\n678")
+    
+    # Test actionsF as shown in class assignment details
+    assert(actionsF(state) == ['left', 'right', 'up', 'down'])
+    state = [1,0,3, 4,2,5, 6,7,8]
+    assert(actionsF(state) == ['left', 'right', 'down'])
+    
+    # Test takeActionF as shown in class assignment details
+    state = [1,0,3, 4,2,5, 6,7,8]
+    assert(takeActionF(copy(state), 'left') == [0,1,3, 4,2,5, 6,7,8])
+    state = [1,0,3, 4,2,5, 6,7,8]
+    assert(takeActionF(copy(state), 'right') == [1,3,0, 4,2,5, 6,7,8])
+    state = [1,0,3, 4,2,5, 6,7,8]
+    assert(takeActionF(copy(state), 'down') == [1,2,3, 4,0,5, 6,7,8])
+    state = [1,2,3, 4,0,5, 6,7,8]
+    assert(takeActionF(copy(state), 'up') == [1,0,3, 4,2,5, 6,7,8])
+    
+    # Setup a test state and goal
+    start = [1,2,3, 4,0,5, 6,7,8]
+    goal =  [1,2,3, 4,5,8, 6,0,7]
+    def goalTestFunction(state):
+        return state == goal
+
+    
+        
--- assign2/search.py
+++ assign2/search.py
@@ -11,6 +11,47 @@
             The iterative deepening search algorithm. 
 '''
 
+#
+# The example graph given in the assignment description.
+#
 
+successors = {
+    'a':  ['b', 'c', 'd'],
+    'b':  ['e', 'f', 'g'],
+    'c':  ['a', 'h', 'i'],
+    'd':  ['j', 'z'],
+    'e':  ['k', 'l'],
+    'g':  ['m'],
+    'k':  ['z']
+}
+
+
+def iterativeDeepeningSearch(startState, actionsF, takeActionF, goalTestF, maxDepth):
+    if goalTestF:
+        return []
+    if maxDepth == 0:
+        return "cutoff"
+    for action in actionsF():
+        childState = takeActionF(startState, action)
+        result = depthLimitedSearchHelper(childState, actionsF, takeActionF, goalTestF, maxDepth-1)
+        if result is "cutoff":
+            cutoffOccurred = True
+        else:
+            if result is not "failure":
+                return result
+    if cutoffOccurred:
+        return "cutoff"
+    else:
+        return "failure"
+
+
+def depthLimitedSearchHelper(state, actionsF, takeActionF, goalTestF, depthLimit) :
+    for depth in range(maxDepth):
+        result = depthLimitedSearchHelper(startState, actionsF, takeActionF, goalTestf, depth)
+        if result is not "cutoff":     
+            return result
+    return "cutoff"
+
+    
 if __name__ == '__main__':
     pass
